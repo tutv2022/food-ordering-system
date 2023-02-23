3c55e9f6fe0d6f4099ac67fe0259a23ac27dc5ab
tutv2022 <tucute89@gmail.com>
2023-02-23 11:02:22 +0700

    Restaurant2: Check in domain

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/entity/Restaurant.java
@@ -0,0 +1,69 @@
+package com.food.ordering.system.restaurant.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.AggregateRoot;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+
+public class Restaurant extends AggregateRoot<RestaurantId> {
+
+    private OrderApproval orderApproval;
+    private boolean active;
+    private final OrderDetail orderDetail;
+
+    private Restaurant(Builder builder) {
+        super.setId( builder.restaurantId );
+        orderApproval = builder.orderApproval;
+        active = builder.active;
+        orderDetail = builder.orderDetail;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    public OrderApproval getOrderApproval() {
+        return orderApproval;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public OrderDetail getOrderDetail() {
+        return orderDetail;
+    }
+
+    public static final class Builder {
+        private RestaurantId restaurantId;
+        private OrderApproval orderApproval;
+        private boolean active;
+        private OrderDetail orderDetail;
+
+        private Builder() {
+        }
+
+        public Builder restaurantId(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder orderApproval(OrderApproval val) {
+            orderApproval = val;
+            return this;
+        }
+
+        public Builder active(boolean val) {
+            active = val;
+            return this;
+        }
+
+        public Builder orderDetail(OrderDetail val) {
+            orderDetail = val;
+            return this;
+        }
+
+        public Restaurant build() {
+            return new Restaurant(this);
+        }
+    }
+}
---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/valueobject/OrderApprovalId.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.restaurant.service.domain.valueobject;
+
+public class OrderApprovalId {
+}
